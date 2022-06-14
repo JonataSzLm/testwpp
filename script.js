066c31f4af2d9bc391578a5830d5748b20eefd32
JonataSzLm <jonaatslima5@gmail.com>
2022-06-14 20:22:56 -0300

    finalização

--- script.js
+++ script.js
@@ -1,16 +1,26 @@
-//https://api.whatsapp.com/send?phone=
-//fazer funcionalidade para enviar na aplicação tb
-//fazer percistencia de dados dos nomes e numeros
-//add favicon e novo nome
-//melhorar design da pag
+let url_web = 'https://web.whatsapp.com/send?phone="';
+let url_pc = 'https://api.whatsapp.com/send?phone="'
 
 function enviarNumero() {
-	var telefone = 0;
-	var url = 'https://web.whatsapp.com/send?phone="';
 	let prefixo = document.getElementById('prefixo');
 	let ddd = document.getElementById('ddd');
 	let numero = document.getElementById('numero');
-	telefone = prefixo.value + ddd.value + numero.value;
+	let radio = document.getElementsByName('whatsapp');
+
+
+	for (var i = 0; i < radio.length; i++) {
+		if (radio[i].checked) {
+			var wpp = radio[i].value;
+		}
+	}
+
+	if (wpp == 'web') {
+		var url = url_web;
+	} else if (wpp == 'pc') {
+		var url = url_pc;
+	}
+
+	var telefone = prefixo.value + ddd.value + numero.value;
 	url += telefone + '"';
 	window.open(url, '_blank');
 }

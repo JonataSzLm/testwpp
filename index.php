0ba6bfe422335578d676f8eacff1489722510a8f
JonataSzLm <jonaatslima5@gmail.com>
2022-06-16 16:00:26 -0300

    adição do historico

--- index.php
+++ index.php
@@ -9,26 +9,30 @@
 	<meta name="msapplication-TileColor" content="#ffffff">
 	<meta name="theme-color" content="#ffffff">
 </head>
-<body>
+<body onload="pegaContatos()">
 	<div class="container">
-		<img src="img/icone.svg" class="icone">
-		<p class="titulo">Conversar com:</p>
-		<form onsubmit="enviarNumero()" id="formulario" name="formulario" class="formulario">
-			<div>
-				<input type="radio" name="whatsapp" id="WebWhatsapp" value="web" class="radio" checked>
-				<label for="WebWhatsapp">Whatsapp Web</label>
-				<input type="radio" name="whatsapp" id="PCWhatsapp" value="pc" class="radio">
-				<label for="PCWhatsapp">Whatsapp para computador</label>
-			</div>
-			<div>
-				<input type="text" name="prefixo" id="prefixo" value="+55" class="caixa">
-				<input type="number" name="ddd" id="ddd" min="0" max="100" value="37"
-			class="caixa">
-				<input type="number" name="numero" id="numero"
-			class="caixa" min="10000000" max="999999999" value="9">
-			</div>
-			<input type="submit" value="INICIAR CONVERSA" id="botao">
-		</form>
+		<div class="send">
+			<img src="img/icone.svg" class="icone">
+			<p class="titulo">Conversar com:</p>
+			<form onsubmit="enviarNumero()" id="formulario" name="formulario" class="formulario">
+				<div>
+					<input type="radio" name="whatsapp" id="WebWhatsapp" value="web" class="radio" checked>
+					<label for="WebWhatsapp">Whatsapp Web</label>
+					<input type="radio" name="whatsapp" id="PCWhatsapp" value="pc" class="radio">
+					<label for="PCWhatsapp">Whatsapp para computador</label>
+				</div>
+				<input type="text" name="nome" id="nome" class="caixa" placeholder="Nome do contato">
+				<div>
+					<input type="text" name="prefixo" id="prefixo" value="+55" class="caixa">
+					<input type="number" name="ddd" id="ddd" min="0" max="100" value="37"
+				class="caixa">
+					<input type="number" name="numero" id="numero"
+				class="caixa" min="10000000" max="999999999" value="9">
+				</div>
+				<input type="submit" value="INICIAR CONVERSA" id="botao">
+			</form>
+		</div>
+		<div class="history" id="history"></div>
 	</div>
 </body>
 </html>
--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 let url_web = 'https://web.whatsapp.com/send?phone="';
-let url_pc = 'https://api.whatsapp.com/send?phone="'
+let url_pc = 'https://api.whatsapp.com/send?phone="';
+
 
 function enviarNumero() {
+	let nome = document.getElementById('nome');
 	let prefixo = document.getElementById('prefixo');
 	let ddd = document.getElementById('ddd');
 	let numero = document.getElementById('numero');
@@ -22,5 +24,52 @@ function enviarNumero() {
 
 	var telefone = prefixo.value + ddd.value + numero.value;
 	url += telefone + '"';
+
+	var tam = localStorage.length;
+	localStorage.setItem(tam + 1, nome.value);
+	localStorage.setItem(tam + 2, telefone);
+
 	window.open(url, '_blank');
+}
+
+function pegaContatos() {
+	let history = document.getElementById('history');
+	var tamanho = localStorage.length;
+	if (tamanho > 1) {
+		history.innerHTML = '<p class="titulo">Historico:</p>';
+		for (let i = 1; i <= tamanho; i++) {
+			if (i % 2 === 0) {
+				var nomeh = localStorage.getItem(i-1);
+				var numeroh = localStorage.getItem(i);
+				history.innerHTML += '<div class="contato">' +
+					'<div class="dados" onclick="addContato(' + i + ')">' +
+						'<p class="titulo2" id="nomeh">' + nomeh + '</p>' +
+						'<p class="subtitulo" id="numeroh">' + numeroh + '</p>' +
+					'</div>' +
+					'<img src="img/lixeira.svg" class="lixo" onclick="excluiContato(' + i + ')">' +
+				'</div>';
+			}
+		}
+	} else {
+		history.innerHTML += '<p class="titulo2">Ainda não há registro de contatos:</p>'
+	}
+}
+
+function excluiContato(num) {
+	localStorage.removeItem(num - 1);
+	localStorage.removeItem(num);
+	document.location.reload(true);
+}
+
+function addContato(num) {
+	let nome = document.getElementById('nome');
+	let prefixo = document.getElementById('prefixo');
+	let ddd = document.getElementById('ddd');
+	let numero = document.getElementById('numero');
+	let radio = document.getElementsByName('whatsapp');
+	nome.value = localStorage.getItem(num - 1);
+	var telefone = localStorage.getItem(num);
+	prefixo.value = telefone.slice(0, 3);
+	ddd.value = telefone.slice(3, 5);
+	numero.value = telefone.slice(5, 14);
 }
